a101edf0374844d2f5bbb78ba13fd0ef2c4e3353
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2021-11-11 16:35:01 +0100

    update Album

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -1,8 +1,14 @@
 import { Card, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { services } from "../..";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Album = ({ album }) => {
+  const navigateTo = useNavigate();
+  const redirectToAlbumDetail = () => {
+    navigateTo("/album/" + album.id);
+  }
 
   return (
     <div>
@@ -10,7 +16,8 @@ const Album = ({ album }) => {
         <Card>
           <Card.Img variant="top" src={album.images.length > 0 ? album.images[0].url : ""}></Card.Img>
           <Card.Body>
-            <Card.Title>{album.name} - <Link to={"./album/" + album.id}>detail</Link></Card.Title>
+            {/* <Card.Title>{album.name} - <Link to={"./album/" + album.id}>detail</Link></Card.Title> */}
+            <Card.Title>{album.name} - <a href="#" onClick={redirectToAlbumDetail}>detail</a></Card.Title>
             <Card.Text>
               Artiste : {album.artists.map((artist, i) => (artist.name + (album.artists.length < i ? ", " : "")))}
             </Card.Text>
